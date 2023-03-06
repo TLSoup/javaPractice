c8d6c04fbdd407980f1816c2f6fc47443af0adc8
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-06 09:32:23 -0700

    main challenge

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -3,15 +3,35 @@ public class MainChallenge {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 10000;
+        int score = 800;
         int levelCompleted = 8;
-        int bonus = 200;
+        int bonus = 100;
 
-       int finalScore = score;
+        int finalScore = score;
+
+        calculateScore(true, 800, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
         //Line below is the same as (gameOver == true)
-       if (gameOver) {
-           finalScore += (levelCompleted * bonus);
-           System.out.println("Your final score was " + finalScore);
-       }
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+        //Line below is the same as (gameOver == true)
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
