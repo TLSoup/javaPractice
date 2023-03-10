1ddf51eca66ab44b336e76ac2ea0247b93caaa52
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-10 16:28:58 -0700

    Equal Sum checker added main method

--- EqualSumChecker/src/EqualSumChecker.java
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -1,5 +1,8 @@
 public class EqualSumChecker {
 
+    public static void main(String[] args) {
+        hasEqualSum(1, 2, 3);
+    }
     public static boolean hasEqualSum(int num1, int num2, int num3) {
 
         return num1 + num2 == num3;
