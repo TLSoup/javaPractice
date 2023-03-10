2365f1281ff5e7c9c111fa83645966e452d3a6b4
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-10 16:30:11 -0700

    added main method to teen number checker

--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,5 +1,10 @@
 public class TeenNumberChecker {
 
+    public static void main(String[] args) {
+        hasTeen(5, 7, 19);
+        isTeen(14);
+    }
+
     public static boolean hasTeen(int num1, int num2, int num3) {
         return (num1 >= 13 && num1 <= 19) ||
                 (num2 >= 13 && num2 <= 19) ||
