894c0d16fd3bbdeb22f8f624115edc5ccb936472
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-20 16:49:12 -0600

    while loops

---
+++ CodingExercises/src/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=5; i++) {
+            System.out.println(i);
+        }
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("J = "+j);
+            j++;
+        }
+    }
+}
