54d3a656024f5e311426127b14e4089fe48c163e
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-20 16:58:49 -0600

    while statments cont.

--- CodingExercises/src/WhileLoop.java
+++ CodingExercises/src/WhileLoop.java
@@ -5,9 +5,22 @@ public class WhileLoop {
             System.out.println(i);
         }
         int j = 1;
-        while (j <= 5) {
+        while (true) {
+            if (j > 5) {
+                break;
+            }
             System.out.println("J = "+j);
             j++;
         }
+        int k = 1;
+        boolean isReady = false;
+        do {
+            if (k > 5) {
+                break;
+            }
+            System.out.println("k = " + k);
+            k++;
+            isReady = (k > 0);
+        } while (isReady);
     }
 }
