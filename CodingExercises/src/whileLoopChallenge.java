49e3408600cac2a812de3b70893a45811a26ee4d
Tracy Campbell <tracy.campbell@bluefcu.com>
2023-03-22 09:43:07 -0600

    while loop challenge

--- CodingExercises/src/whileLoopChallenge.java
+++ CodingExercises/src/whileLoopChallenge.java
@@ -1,2 +1,22 @@
 public class whileLoopChallenge {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int finishNumber = 20;
+
+        while(number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
+    }
+    public static boolean isEvenNumber(int number) {
+        if ((number % 2) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
